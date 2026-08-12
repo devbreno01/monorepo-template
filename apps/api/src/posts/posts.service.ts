19abc2ef85ba45e8066c99f1cd0b2a0b6a4ed32b
Breno Guimarães <devbreno4@gmail.com>
2026-08-11 22:07:58 -0300

    feat: create post route

--- apps/api/src/posts/posts.service.ts
+++ apps/api/src/posts/posts.service.ts
@@ -1,26 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class PostsService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+
+  constructor(private prisma: PrismaService){}
+
+  async create(createPostDto: CreatePostDto) {
+      const post = this.prisma.post.create({
+        data:{
+          title: createPostDto.title, 
+          content: createPostDto.content, 
+          published: createPostDto.published, 
+          author_id: createPostDto.author_id, 
+          tenant_id: createPostDto.tenant_id
+        },
+        select:{
+          title: true, 
+          content: true
+        }
+      }); 
+
+      return post; 
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all posts`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} post`;
   }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
+  async update(id: number, updatePostDto: UpdatePostDto) {
     return `This action updates a #${id} post`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} post`;
   }
 }
